dea0669edaf7aa3a1fdac50a3e6524c42c875728
Juliano Fernandes <julianofernandes@gmail.com>
2026-03-03 17:26:25 -0300

    Add acceptance tests for importer post-processor

---
+++ post-processor/magalucloud/import_acc_test.go
@@ -0,0 +1,63 @@
+// Copyright (c) Juliano Fernandes 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package magalucloud
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/packer-plugin-sdk/acctest"
+)
+
+//go:embed test-fixtures/template.pkr.hcl
+var testImporterHCL2 string
+
+// Run with: PACKER_ACC=1 go test -count 1 -v ./post-processor/magalucloud/import_acc_test.go  -timeout=120m
+func TestAccMagaluCloudImporter(t *testing.T) {
+	testCase := &acctest.PluginTestCase{
+		Name: "magalucloud_importer_test",
+		Setup: func() error {
+			return nil
+		},
+		Teardown: func() error {
+			return nil
+		},
+		Template: testImporterHCL2,
+		Type:     "magalucloud",
+		Check: func(buildCommand *exec.Cmd, logfile string) error {
+			if buildCommand.ProcessState != nil {
+				if buildCommand.ProcessState.ExitCode() != 0 {
+					return fmt.Errorf("bad exit code. logfile: %s", logfile)
+				}
+			}
+
+			data, err := os.ReadFile(logfile)
+			if err != nil {
+				return fmt.Errorf("unable to read %s", logfile)
+			}
+
+			logs := string(data)
+			expectedLogs := []string{
+				"test.file.image (magalucloud-import): Uploading",
+				"test.file.image (magalucloud-import): Generating presigned URL",
+				"test.file.image (magalucloud-import): Importing image",
+				"test.file.image (magalucloud-import): Finished importing image",
+				"test.file.image (magalucloud-import): Deleting",
+			}
+
+			for _, expected := range expectedLogs {
+				if !strings.Contains(logs, expected) {
+					t.Fatalf("logs doesn't contain expected value %s", expected)
+				}
+			}
+
+			return nil
+		},
+	}
+	acctest.TestPlugin(t, testCase)
+}
---
+++ post-processor/magalucloud/test-fixtures/template.pkr.hcl
@@ -0,0 +1,55 @@
+# Copyright (c) Juliano Fernandes 2026
+# SPDX-License-Identifier: MPL-2.0
+
+variable "api_key" {
+  type      = string
+  default   = env("MGC_API_KEY")
+  sensitive = true
+}
+
+variable "access_key" {
+  type      = string
+  default   = env("MGC_ACCESS_KEY")
+  sensitive = true
+}
+
+variable "secret_key" {
+  type      = string
+  default   = env("MGC_SECRET_KEY")
+  sensitive = true
+}
+
+variable "region" {
+  type    = string
+  default = "br-se1"
+}
+
+variable "bucket" {
+  type    = string
+  default = env("BUCKET")
+}
+
+variable "name_prefix" {
+  type    = string
+  default = "packer-testacc"
+}
+
+source "file" "image" {
+  source = "test-fixtures/image.qcow2"
+  target = "test-fixtures/test.qcow2"
+}
+
+build {
+  name = "test"
+
+  sources = ["source.file.image"]
+
+  post-processor "magalucloud-import" {
+    api_key    = var.api_key
+    access_key = var.access_key
+    secret_key = var.secret_key
+    region     = var.region
+    bucket     = var.bucket
+    image_name = "${var.name_prefix}-${formatdate("YYYYMMDDhhmmss", timestamp())}"
+  }
+}
